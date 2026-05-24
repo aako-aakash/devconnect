27aabe7b58752f4d37dc610ec876f7b693509b1e
aakoaakash <aako27082004@google.com>
2026-05-25 01:20:10 +0530

    fixed calling url

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
+        "https://devconnect-steel.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
